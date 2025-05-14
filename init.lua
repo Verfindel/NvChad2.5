3ef720955d340c3903d15e9a2d9a1e1d0f6ef60f
Are Johan Litleskare <arejohan.litleskare@stormgeo.com>
2025-05-14 21:26:17 +0200

    minor fixes

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
+vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,44 +1,46 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+-- local on_attach = require("nvchad.configs.lspconfig").on_attach
+-- local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 require("mason").setup()
-local mason_lspconfig = require "mason-lspconfig"
-local lspconfig = require "lspconfig"
-
-mason_lspconfig.setup_handlers({
-  function (server_name)
-    lspconfig[server_name].setup({
-      on_attach = on_attach,
-      capabilities = capabilities,
-    })
-  end,
-  ["lua_ls"] = function()
-		local lua_runtime_path = vim.split(package.path, ";")
-		table.insert(lua_runtime_path, "lua/?.lua")
-		table.insert(lua_runtime_path, "lua/?/init.lua")
-
-		require("lspconfig")["lua_ls"].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-						path = lua_runtime_path,
-					},
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-						checkThirdParty = false,
-					},
-					telemetry = {
-						enable = false,
-					},
-				},
-			},
-		})
-	end,
-})
+require("mason-lspconfig").setup()
+-- local mason_lspconfig = require "mason-lspconfig"
+-- local lspconfig = require "lspconfig"
+-- 
+-- 
+-- mason_lspconfig.setup_handlers({
+--   function (server_name)
+--     lspconfig[server_name].setup({
+--       on_attach = on_attach,
+--       capabilities = capabilities,
+--     })
+--   end,
+--   ["lua_ls"] = function()
+-- 		local lua_runtime_path = vim.split(package.path, ";")
+-- 		table.insert(lua_runtime_path, "lua/?.lua")
+-- 		table.insert(lua_runtime_path, "lua/?/init.lua")
+-- 
+-- 		require("lspconfig")["lua_ls"].setup({
+-- 			on_attach = on_attach,
+-- 			capabilities = capabilities,
+-- 
+-- 			settings = {
+-- 				Lua = {
+-- 					runtime = {
+-- 						version = "LuaJIT",
+-- 						path = lua_runtime_path,
+-- 					},
+-- 					diagnostics = {
+-- 						globals = { "vim" },
+-- 					},
+-- 					workspace = {
+-- 						library = vim.api.nvim_get_runtime_file("", true),
+-- 						checkThirdParty = false,
+-- 					},
+-- 					telemetry = {
+-- 						enable = false,
+-- 					},
+-- 				},
+-- 			},
+-- 		})
+-- 	end,
+-- })
